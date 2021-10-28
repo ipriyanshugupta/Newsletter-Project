4b5d0e795009092db5c2a94ffe4581b73a552ddd
Priyanshu Gupta <priyanshu.gupta@mfine.co>
2021-10-28 22:52:54 +0530

    making some changes

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/', function(req,res){
     const url = "https://us1.api.mailchimp.com/3.0/lists/50b0ac2afb";
     const options= {
         method: "post",
-        auth: "priyanshu:fbdcb147bb8170e1d36da2cc245f2d82-us1"
+        auth: "priyanshu:b5f0f267f9dcdca39d295f9129bf29cc-us1"
     }
     const request = https.request(url,options,function(response){
         if(response.statusCode===200)
@@ -62,6 +62,6 @@ app.listen(port,()=>{
 })
 
 // API key
-// fbdcb147bb8170e1d36da2cc245f2d82-us1
+// b5f0f267f9dcdca39d295f9129bf29cc-us1
 // list id
-// 50b0ac2afb   s
+// 50b0ac2afb 
